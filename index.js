c8040860d12107795f0c77a0fc3adc2a488b0d92
Mathew Phillip Wheatley <mathew.p.wheatley@gmail.com>
2020-08-11 19:21:48 -0700

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,20 @@
 function findMinAndRemove(array){
-
+    let currentMin = array[0]
+    let minIndex = 0
+    for(let i = 0; i < array.length; i++){
+      if(array[i] < currentMin){
+        currentMin = array[i]
+        minIndex = i
+      }
+    }
+    array.splice(minIndex, 1);
+    return currentMin;
 }
 
 function selectionSort(array){
-
+    const sorted = []
+    while (array.length != 0) {
+        sorted.push(findMinAndRemove(array))
+    }
+    return sorted
 }
