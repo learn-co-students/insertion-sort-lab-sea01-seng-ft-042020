406bb1fc82c82414878a434c60a8a2a75db8dacc
Justin Langlinais <justinclanglinais@gmail.com>
2020-08-28 11:06:37 -0700

    solving

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-function findMinAndRemove(array){
-
+function findMinAndRemove(array) {
+    let min = array[0]
+    let minIndex = 0
+    for ( i = 0 ; i < array.length ; i++ ) {
+        if ( array[i] < min ) {
+            min = array[i]
+            minIndex = i
+        }
+    }
+    array.splice( minIndex , 1 )
+    return min
 }
 
-function selectionSort(array){
-
+function selectionSort(array) {
+    let newMin
+    let sortedArr = []
+    while ( array.length != ) {
+        newMin = findMinAndRemove(array)
+        sortedArr.push(newMin)
+    }
+    return sortedArr
 }
